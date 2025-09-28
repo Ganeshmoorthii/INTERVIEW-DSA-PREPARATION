b0f871a76589b100a68355206a4f38f18c570e23
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-09-28 11:15:30 +0530

    Update Valid Palindrome.java

--- TWO POINTER/Valid Palindrome.java
+++ TWO POINTER/Valid Palindrome.java
@@ -1,1 +1,55 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+
+*/
+class Solution {
+    public boolean isPalindrome(String s) { 
+        int l = 0,r= s.length()-1;
+        while(l<r){
+            char lef = s.charAt(l);
+            char rig = s.charAt(r);
+            if(!Character.isLetterOrDigit(lef)){
+                l++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(rig)){
+                r--;
+                continue;
+            }
+            if(Character.toLowerCase(lef)!=Character.toLowerCase(rig)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
